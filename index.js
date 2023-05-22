300567891145e175f123c1e7991a1697322e7c37
MikaëlLESGUER <mikedev35@outlook.fr>
2023-05-22 08:39:53 +0000

    excercice 1

---
+++ index.js
@@ -0,0 +1,17 @@
+const character= "legolas";
+// character = "Gimly";
+console.log(character);
+
+let name= "legolas";
+name = "Gimly";
+console.log(name);
+
+
+let x = 345521;
+let y = -6753; 
+let z = 4.23021; 
+console.log(x,y,z)
+
+let truth = true; 
+let lie = false; 
+console.log(truth, lie)
